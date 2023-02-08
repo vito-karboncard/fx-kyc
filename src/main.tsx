1d03a454bdf8955dd156cbc6992f3b3f2bd9f357
Vito Liu <vito@karboncard.com>
2023-02-08 12:01:38 +0800

    add Antd theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
-
+import {ConfigProvider} from 'antd'
+import AntdTheme from './config/Ant Design Theme.json'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={AntdTheme}>
+      <ConfigProvider theme={{
+        inherit: true,
+        components: {
+          Button: {
+            paddingContentHorizontal: 24,
+            controlHeight: 40,
+            paddingContentHorizontalLG: 24,
+            controlHeightLG: 44,
+            paddingContentHorizontalSM: 24,
+            controlHeightSM: 24,
+          }
+        }
+      }}>
+        <App />
+      </ConfigProvider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
