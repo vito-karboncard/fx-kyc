fe4d748a58f381e73f0c8d4284d9f3658caf055d
Vito Liu <vito@karboncard.com>
2023-02-08 12:24:18 +0800

    IDE config

---
+++ src/AppProvider.tsx
@@ -0,0 +1,25 @@
+import React, {ReactNode} from 'react'
+import AntdTheme from './config/Ant Design Theme.json'
+import {ConfigProvider} from 'antd'
+
+function AppProvider({children}: {children: ReactNode}) {
+  return <ConfigProvider theme={AntdTheme}>
+    <ConfigProvider theme={{
+      inherit: true,
+      components: {
+        Button: {
+          paddingContentHorizontal: 24,
+          controlHeight: 40,
+          paddingContentHorizontalLG: 24,
+          controlHeightLG: 44,
+          paddingContentHorizontalSM: 24,
+          controlHeightSM: 24,
+        }
+      }
+    }}>
+      {children}
+    </ConfigProvider>
+  </ConfigProvider>
+}
+
+export default AppProvider
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {ConfigProvider} from 'antd'
-import AntdTheme from './config/Ant Design Theme.json'
+import AppProvider from 'src/AppProvider'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ConfigProvider theme={AntdTheme}>
-      <ConfigProvider theme={{
-        inherit: true,
-        components: {
-          Button: {
-            paddingContentHorizontal: 24,
-            controlHeight: 40,
-            paddingContentHorizontalLG: 24,
-            controlHeightLG: 44,
-            paddingContentHorizontalSM: 24,
-            controlHeightSM: 24,
-          }
-        }
-      }}>
-        <App />
-      </ConfigProvider>
-    </ConfigProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
 )
