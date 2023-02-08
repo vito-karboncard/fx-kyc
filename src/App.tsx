74d5b45ff5dadac7d12980eb469decfc4b89ed7b
Vito Liu <vito@karboncard.com>
2023-02-08 13:52:48 +0800

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Button } from "antd";
 import "./style/App.scss";
 import Style from "./style/override-antd.global.module.scss";
 import classNames from "classnames";
+import "./style/tailwind.css";
 
 function App() {
   return (
     <div className={classNames("App", Style.overrideAntd)}>
-      <p>
+      <p className={"my-2"}>
         <Button size={"small"}>Antd button</Button>
         <Button size={"middle"} type={"primary"} ghost>
           Antd button
